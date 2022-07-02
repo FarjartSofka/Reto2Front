aa902bf31b23a4b53ad166fe5ca5e4cbbbff878a
Farly Artunduaga <farly.artunduaga@sofka.com.co>
2022-07-01 23:35:08 -0500

    Se incluye MaterialCss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { PistaComponent } from './shared/pista/pista.component';
 import { SharedModule } from './shared/shared.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { CrearJuegoComponent } from './pages/crear-juego/crear-juego.component';
+import { IniciarJuegoComponent } from './pages/iniciar-juego/iniciar-juego.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    CrearJuegoComponent,
+    IniciarJuegoComponent,
 
   ],
   imports: [
     SharedModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
