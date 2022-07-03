044577cee704a339819186166e7b504cefbb5b1a
Farly Artunduaga <farly.artunduaga@sofka.com.co>
2022-07-02 21:32:29 -0500

    Generacion Servicio login Google

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { JuegoFormComponent } from './juego-form/juego-form.component';
+
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
-  {path:'', component: JuegoFormComponent}
+  {path:'', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,21 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { FormsModule } from '@angular/forms';
 import { PagesRoutingModule } from './pages-routing.module';
 import { JuegoFormComponent } from './juego-form/juego-form.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
+import { LoginComponent } from './login/login.component';
 
 
 @NgModule({
   declarations: [
-    JuegoFormComponent
+    JuegoFormComponent,
+    LoginComponent
   ],
   imports: [
     SharedModule,
     CommonModule,
     PagesRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule, 
+    FormsModule
   ]
 })
 export class PagesModule { }
