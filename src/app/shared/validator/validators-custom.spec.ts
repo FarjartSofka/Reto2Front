00f9960e2ae934abb5b8fc85b5b887fb0d4cc8ed
emanuelvallejoc <emanuel.vallejo@sofka.com.co>
2022-07-05 11:23:26 -0500

    se agregan validación al formulario de crear juego y se entrega podio final en modal

---
+++ src/app/shared/validator/validators-custom.spec.ts
@@ -0,0 +1,7 @@
+import {  ValidatorsCustom } from './validators-custom';
+
+describe('ValidatorsCustom', () => {
+  it('should create an instance', () => {
+    expect(new ValidatorsCustom()).toBeTruthy();
+  });
+});
